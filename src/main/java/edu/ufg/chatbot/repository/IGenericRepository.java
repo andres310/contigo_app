b0e8b64e9a602cac03aa1b19dd1c242a681274b2
Andrés <ia.andresalexander@ufg.edu.sv>
2024-10-24 20:21:52 -0600

    init

--- src/main/java/edu/ufg/chatbot/repository/IGenericRepository.java
+++ src/main/java/edu/ufg/chatbot/repository/IGenericRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface IGenericRepository<T, ID> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
+
 }
